3364b03dd2d77ec4c40081da778b5f03d0a25d17
Marcia Satie <111194629+MarciaSatie@users.noreply.github.com>
2025-05-07 21:46:55 +0100

    header showing current date

--- js/pages/header.js
+++ js/pages/header.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const time = dotify.utils.currentTimeInfo();
+    const now = dayjs();
+    const currentHour = now.hour()%12;
+    const currentDate = now.format('DD [of] MMMM');
     const weekDay = dayjs().add(2, 'day').format('dddd');
     const weekDayElement = document.getElementById('weekday');
     if(weekDayElement){ // if is not null will run the code (to avoid error when at preferences remove this tag)
-      weekDayElement.textContent = `${weekDay}  -  ${time.hour12}:${time.min} ${time.ampm}`;
+      weekDayElement.textContent = `${currentDate}  ( ${weekDay}) -  ${currentHour}:${time.min} ${time.ampm}`;
     }
   });
