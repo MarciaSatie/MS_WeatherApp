54cffabb0f1b15c063b22a5e79a7bb7894b0539b
Marcia Satie <111194629+MarciaSatie@users.noreply.github.com>
2025-05-10 18:08:32 +0100

    Fixing minutes in the header to show '0' when currentMinutes is <10

--- js/pages/header.js
+++ js/pages/header.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const now = dayjs();
     const currentHour = now.hour()%12;
     const currentDate = now.format('DD [of] MMMM');
-    const currentMinute = now.minute();
+    let currentMinute = now.minute();
+    if(currentMinute<10)currentMinute='0'+currentMinute;
+    
     const weekDay = now.format("dddd")
     const weekDayElement = document.getElementById('weekday');
     const currentTimeDiv = document.getElementById('headerTime');
