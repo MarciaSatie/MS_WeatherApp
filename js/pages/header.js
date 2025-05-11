c0013e5db9265e9f78dffed99bb5ce31055fbd0b
Marcia Satie <111194629+MarciaSatie@users.noreply.github.com>
2025-05-11 13:13:06 +0100

    change in the header fixinf midday

--- js/pages/header.js
+++ js/pages/header.js
@@ -1,11 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const time = dotify.utils.currentTimeInfo();
     const now = dayjs();
-<<<<<<< HEAD
     const currentHour = (now.hour())?now.hour()%12:now.hour();
-=======
-    const currentHour = (now.hour()!==12)?now.hour()%12:now.hour();
->>>>>>> 62ca60c8c8f7abebfb5427eeaee6277acbaf902d
     const currentDate = now.format('DD [of] MMMM');
     let currentMinute = now.minute();
     if(currentMinute<10)currentMinute='0'+currentMinute;
